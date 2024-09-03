aa6a6980be9aff7824363d48384cd4ebf3c0574f
Loc Le <lpl212757@gmail.com>
2024-09-03 14:53:23 +0700

    feat - implement MapManual

--- src/LocQ/Projection/Map.cs
+++ src/LocQ/Projection/Map.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 public static class MapExtension
 {
     public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> source,
@@ -14,4 +16,85 @@ public static class MapExtension
             yield return selector(item);
         }
     }
+
+    public static IEnumerable<TResult> MapManual<TSource, TResult>(this IEnumerable<TSource> source,
+                                                              Func<TSource, TResult> selector)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return new MapEnumerable<TSource, TResult>(source, selector);
+    }
+
+    private class MapEnumerable<TSource, TResult> : IEnumerable<TResult>
+    {
+        private IEnumerable<TSource> _source;
+        private Func<TSource, TResult> _selector;
+
+        public MapEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            this._source = source;
+            this._selector = selector;
+        }
+
+        public IEnumerator<TResult> GetEnumerator()
+        {
+            return new MapEnumerator<TSource, TResult>(_source, _selector);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    private class MapEnumerator<TSource, TResult> : IEnumerator<TResult>
+    {
+        private IEnumerable<TSource> _source;
+        private Func<TSource, TResult> _selector;
+        private TResult _current = default!;
+        private IEnumerator<TSource> _enumerator;
+        private int _state = 1;
+
+        public MapEnumerator(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            this._source = source;
+            this._selector = selector;
+        }
+
+        public TResult Current => _current;
+
+        object IEnumerator.Current => Current!;
+
+        public bool MoveNext()
+        {
+            switch (_state)
+            {
+                case 1:
+                    _enumerator = _source.GetEnumerator();
+                    _state = 2;
+                    goto case 2;
+                case 2:
+                    if(_enumerator.MoveNext())
+                    {
+                        _current = _selector(_enumerator.Current);
+                        return true;
+                    }
+                    return false;
+            }
+
+            Dispose();
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Dispose()
+        {
+            _enumerator?.Dispose();
+            _state = -1;
+        }
+
+    }
 }
+
--- tests/LocQ.Tests/FunctionTests/MapTest.cs
+++ tests/LocQ.Tests/FunctionTests/MapTest.cs
@@ -58,4 +58,60 @@ public class MapTest
           Assert.Equal(data[i].Age, listAges[i]);
       }
     }
+
+    [Fact]
+    public void MapManual_GiveListOfNumber_WhenMapMethodIsCalledWithSquareFunction_ThenReturnListOfSquareNumber()
+    {
+        //Arrange
+        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        //Act
+        var result = numbers.MapManual(x => x * x);
+
+        //Assert
+        Assert.Equal(10, result.Count());
+        Assert.Equal(new List<int> { 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 }, result);
+    }
+
+    [Fact]
+    public void MapManual_GiveListOfString_WhenMapMethodIsCalledConcatWithHelloPrefix_ThenReturnListOfStringWithHelloPrefix()
+    {
+        //Arrange
+        IEnumerable<string> data = new string[] { "World", "LocQ", "C#", "Dotnet" };
+
+        //Act
+        var result = data.MapManual(x => "Hello " + x);
+
+        //Assert
+        Assert.Equal(4, result.Count());
+        foreach (var item in result)
+        {
+            Assert.StartsWith("Hello ", item);
+        }
+    }
+
+    [Fact]
+    public void MapManual_GiveListOfObject_WhenMapMethodIsCalled_ReturnListOfObjectWithUpdatedValue()
+    {
+      //Arrange
+      var data = new List<Person>
+      {
+          new Person { Name = "John", Age = 20 },
+          new Person { Name = "Doe", Age = 30 },
+          new Person { Name = "Jane", Age = 40 }
+      };
+
+      //Act
+      var ages = data.MapManual(x => x.Age);
+
+      //Assert
+      Assert.Equal(3, ages.Count());
+
+      int?[] listAges = ages.ToArray();
+      for (int i = 0; i < listAges.Length; i++)
+      {
+          Assert.Equal(data[i].Age, listAges[i]);
+      }
+    }
+
 }
