6ddb5337f2919cfa28284e4cd67e72d48c5017ab
Loc Le <lpl212757@gmail.com>
2024-09-05 02:42:29 +0700

    feat - add more unit test for throw expection when null

--- tests/LocQ.Tests/FunctionTests/SortByTest.cs
+++ tests/LocQ.Tests/FunctionTests/SortByTest.cs
@@ -17,4 +17,42 @@ public class SortByTest
         Assert.Equal(5, result.Count());
         Assert.Equal(new List<int>{ 1, 2, 3, 4, 5 }, result);
     }
+
+    [Fact]
+    public void SortBy_GivenListOfStrings_ThenReturnSortedList()
+    {
+        //Arrange
+        var list = new List<string> { "B", "A", "D", "C", "E" };
+        //Act
+        var result = list.SortBy(x => x);
+        //Assert
+        Assert.Equal(5, result.Count());
+        Assert.Equal(new List<string>{ "A", "B", "C", "D", "E" }, result);
+    }
+
+    [Fact]
+    public void SortBy_GivenNullList_ThenThrowArgumentNullException()
+    {
+        //Arrange
+        List<int> list = null;
+
+        //Act
+        Action act = () => list.SortBy(x => x);
+
+        //Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact]
+    public void SortBy_GivenNullSelector_ThenThrowArgumentNullException()
+    {
+        //Arrange
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+
+        //Act
+        Action act = () => SortByExtension.SortBy<int, object>(list, null);
+
+        //Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
 }
